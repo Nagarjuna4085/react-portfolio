22f3b302bbb8de482421a7f0d0d4069f3d577be5
nagarjnasanam <snagarjuna2001@gmail.com>
2025-10-14 16:45:29 -0400

    initialize and project setup

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme logic */}
+      <ThemeToggle />
+
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
