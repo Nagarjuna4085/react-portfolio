c634c73a5b6f777947577db464a83b031a81724a
nagarjnasanam <snagarjuna2001@gmail.com>
2025-10-15 06:35:37 -0400

    About section

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { NavBar } from "@/components/NavBar";
-
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -14,7 +15,13 @@ export const Home = () => {
 
       {/* Navbar */}
       <NavBar />
+
       {/* Main Content */}
+
+      <main>
+        <HeroSection />
+        <AboutSection />
+      </main>
 
       {/* Footer */}
     </div>
